34aaff6733811b05dc03e02859ef845334dddc9d
Miguel <cueiras98@gmail.com>
2018-12-11 11:46:42 +0100

    punto1

---
+++ src/manolete/Miguel.java
@@ -0,0 +1,33 @@
+package manolete;
+
+public class Miguel {
+	int nombre;
+	int apellidos;
+	int dni;
+
+	public Miguel() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getNombre() {
+		return nombre;
+	}
+	public void setNombre(int nombre) {
+		this.nombre = nombre;
+	}
+	public int getApellidos() {
+		return apellidos;
+	}
+	public void setApellidos(int apellidos) {
+		this.apellidos = apellidos;
+	}
+	public int getDni() {
+		return dni;
+	}
+	public void setDni(int dni) {
+		this.dni = dni;
+	}
+
+	
+	
+}
---
+++ src/manolete/Reloj.java
@@ -0,0 +1,40 @@
+package manolete;
+
+public class Reloj {
+	
+	int hora;
+	int minutos;
+	int segundos;
+	
+	
+	public Reloj() {
+		super();
+	}
+
+	public int getHora() {
+		return hora;
+	}
+
+	public void setHora(int hora) {
+		this.hora = hora;
+	}
+
+	public int getMinutos() {
+		return minutos;
+	}
+
+	public void setMinutos(int minutos) {
+		this.minutos = minutos;
+	}
+
+	public int getSegundos() {
+		return segundos;
+	}
+
+	public void setSegundos(int segundos) {
+		this.segundos = segundos;
+	}
+	
+	
+	
+}
