27dac189b22f0ce97ec636dcc7c2b2e468028fd1
Miguel <cueiras98@gmail.com>
2018-12-11 11:58:04 +0100

    punto 2

--- src/manolete/Reloj.java
+++ src/manolete/Reloj.java
@@ -1,5 +1,12 @@
 package manolete;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class Reloj {
 	
 	int hora;
@@ -35,6 +42,28 @@ public class Reloj {
 		this.segundos = segundos;
 	}
 	
-	
+	public void write() {
+		// Copiar ficheros
+		File origen = new File("origen.txt");
+		File destino = new File("destino.txt");
+
+		try {
+		  InputStream in = new FileInputStream(origen);
+		  OutputStream out = new FileOutputStream(destino);
+						
+		  byte[] buf = new byte[1024];
+		  int len;
+
+		  while ((len = in.read(buf)) > 0) {
+		    out.write(buf, 0, len);
+		  }
+				
+		  in.close();
+		  out.close();
+		} catch (IOException ioe){
+		  ioe.printStackTrace();
+		}
+
+	}
 	
 }
